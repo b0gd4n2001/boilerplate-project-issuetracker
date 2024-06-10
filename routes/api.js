612e276c08a74f81f0ff78e84c791b9486a2a5c3
b0gd4n2001 <lulelarub@gmail.com>
2024-06-10 14:31:14 +0300

    feat: log body

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 'use strict';
+const IssueTrackerModel = require('../mongoose_models/models.js')
 
 module.exports = function (app) {
 
@@ -6,21 +7,25 @@ module.exports = function (app) {
   
     .get(function (req, res){
       let project = req.params.project;
+      console.log(req.params, req.query, req.body);
       
     })
     
     .post(function (req, res){
       let project = req.params.project;
+      console.log(req.params, req.query, req.body);
       
     })
     
     .put(function (req, res){
       let project = req.params.project;
+      console.log(req.params, req.query, req.body);
       
     })
     
     .delete(function (req, res){
       let project = req.params.project;
+      console.log(req.params, req.query, req.body);
       
     });
     
